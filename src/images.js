34bfc761ceba02b63d2ed3e2508e83d43fd92744
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-12-29 11:46:22 +0100

    move the images into a separate file to make Hangman.js look cleaner

---
+++ src/images.js
@@ -0,0 +1,9 @@
+import img0 from './images/0.jpg';
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+import img6 from './images/6.jpg';
+
+export { img0, img1, img2, img3, img4, img5, img6 };
