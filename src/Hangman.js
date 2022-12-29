e713ce47be775193b6a62b678daa3707afe8f6c2
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-12-29 11:13:39 +0100

    create Hangman component, wire it up with the app, change the font-style, and add bg and hangman images

---
+++ src/Hangman.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hangman extends Component {
+    render() {
+        return (
+            <div>I am a hangman</div>
+        )
+    }
+}
+
+export default Hangman; 
